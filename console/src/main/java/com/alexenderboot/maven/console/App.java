ceae64c13cd0abeb38ee5a1b5e40509432849345
Sanyandr <alexenderboot@gmail.com>
2022-07-02 19:49:37 +0300

    LMBK004 - nonNull example

--- console/src/main/java/com/alexenderboot/maven/console/App.java
+++ console/src/main/java/com/alexenderboot/maven/console/App.java
@@ -1,12 +1,25 @@
 package com.alexenderboot.maven.console;
 
+import com.alexenderboot.maven.core.NonNullExample;
+import com.alexenderboot.maven.core.Person;
 import com.alexenderboot.maven.core.ValExample;
 import com.alexenderboot.maven.core.VarExample;
 
+import javax.naming.NameNotFoundException;
+
 public class App {
     public static void main(String[] args) {
+        //Lesson 2, val Example
         System.out.println(ValExample.example());
         ValExample.example2();
+        //Lesson 3, var Example
         System.out.println(VarExample.example());
+
+        //Lesson 4, tryin to make NULL object person -> got exception
+        Person person = null;
+        NonNullExample nonNull = new NonNullExample(person);
+        System.out.println(nonNull);
+
+        
     }
 }
---
+++ core/src/main/java/com/alexenderboot/maven/core/Person.java
@@ -0,0 +1,10 @@
+package com.alexenderboot.maven.core;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+    String name;
+}
